9a172d49b3247483fca6a014c84b6b758ad0e8fd
raphatapi <raphaeltapioca10@gmail.com>
2018-11-14 10:05:24 -0700

    fixed scrolling section height

--- script.js
+++ script.js
@@ -2,26 +2,54 @@ $(document).ready(function() {
 
     var main = main = $('.main ul');
     
-    $('.scroll').click(function(event) {
+    // $('.scroll').click(function(event) {
             
-        event.preventDefault();
+    //     event.preventDefault();
     
-        var full_url = this.href,
-            parts = full_url.split('#'),
-            trgt = parts[1],
-            target_offset = $('#'+trgt).offset(),
-            target_top = target_offset.top;
+    //     var full_url = this.href,
+    //         parts = full_url.split('#'),
+    //         trgt = parts[1],
+    //         target_offset = $('#'+trgt).offset(),
+    //         target_top = target_offset.top;
             
-        $('html, body').animate({scrollTop:target_top}, 500);
+    //     $('html, body').animate({scrollTop:target_top}, 500);
         
-        /* Remove active class on any li when an anchor is clicked */
+    //     /* Remove active class on any li when an anchor is clicked */
         
-        main.children().removeClass();
+    //     main.children().removeClass();
         
-        /* Add active class to clicked anchor's parent li */
+    //     /* Add active class to clicked anchor's parent li */
             
-        $(this).parent().addClass('active');
+    //     $(this).parent().addClass('active');
 
-    });
+    // });
+
+    // Add smooth scrolling to all links
+    $(".scroll").on('click', function(event) {
+  
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (1200) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top - $(".navbar").outerHeight(true)
+          }, 1200, function(){
+       
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            // window.location.hash = hash;
+          });
+        } // End if
+      });
+  
+      $('.navbar-brand').on('click', function(){
+        $('html, body').animate({scrollTop : 0},600);
+        return false;
+      });
 
 });
