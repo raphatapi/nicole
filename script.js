e6a1cd4da3e7224e59fa8505788c68f68441dbcb
raphatapi <raphaeltapioca10@gmail.com>
2018-11-13 22:44:38 -0700

    changed navbar and added scroll

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    var main = main = $('#main ul');
+
+$('.scroll').click(function(event) {
+        
+    event.preventDefault();
+ 
+    var full_url = this.href,
+        parts = full_url.split('#'),
+        trgt = parts[1],
+        target_offset = $('#'+trgt).offset(),
+        target_top = target_offset.top;
+        
+    $('html, body').animate({scrollTop:target_top}, 500);
+    
+    /* Remove active class on any li when an anchor is clicked */
+    
+    main.children().removeClass();
+    
+    /* Add active class to clicked anchor's parent li */
+        
+    $(this).parent().addClass('active');
+
+});
+
+});
