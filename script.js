d3cd584b9a794b3e694e9609d48ccaecfe75f346
raphatapi <raphaeltapioca10@gmail.com>
2019-03-26 13:32:00 -0600

    fixed bottom links hash

--- script.js
+++ script.js
@@ -3,42 +3,46 @@ $(document).ready(function() {
   $('.carousel').carousel();
   
 
-     // Add smooth scrolling to all links
+     // Add smooth scrolling to top links
     $(".scroll,.contact").on('click', function(event) {
-  
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
           // Prevent default anchor click behavior
           event.preventDefault();
-    
           // Store hash
-          var hash = this.hash;
-    
+          var hashTop = this.hash;
           // Using jQuery's animate() method to add smooth page scroll
           // The optional number (1200) specifies the number of milliseconds it takes to scroll to the specified area
           $('html, body').animate({
-            scrollTop: $(hash).offset().top - $("nav").outerHeight(true)
-          }, 1200, function(){
-       
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            // window.location.hash = hash;
+              scrollTop: $(hashTop).offset().top - $("nav").outerHeight(true)
+            }, 1200, function(){
           });
         } // End if
       });
   
+
+
+      // Add smooth scrolling to bottom links
+    $(".contact").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+          //Store hash for bottom links
+          var hashBottom = this.hash;
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (1200) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+              scrollTop: $(hashBottom).offset().top
+            }, 1200, function(){
+          });
+        } // End if
+    });
+
       $('.navbar-brand').on('click', function(){
         $('html, body').animate({scrollTop : 0},600);
         return false;
       });
-
-
-      // $(".contact").on("click", function(event) {
-      //   window.setTimeout(function() {
-      //     offsetAnchor();
-      //   }, 0);
-      // });
-
-      // window.setTimeout(offsetAnchor, 0);
 
 
       $(window).scroll(function(){
@@ -106,10 +110,3 @@ function topFunction() {
 function bottomFunction() {
   $("html, body").animate({ scrollTop: document.body.scrollHeight }, 1200);
 }
-
-// When the user clicks on the link, scroll to the top of the hash
-// function offsetAnchor() {
-//   if (location.hash.length !== 0) {
-//     window.scrollTo(window.scrollX, window.scrollY - 70);
-//   }
-// }
