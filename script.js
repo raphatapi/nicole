6110f240f5684e15d500c799834c484f7add406d
raphatapi <raphaeltapioca10@gmail.com>
2019-05-06 23:43:00 -0600

    footer

--- script.js
+++ script.js
@@ -47,19 +47,14 @@ $(document).ready(function() {
 
       // Add smooth scrolling to bottom links
     $(".contact").on('click', function(event) {
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-          // Prevent default anchor click behavior
           event.preventDefault();
-          //Store hash for bottom links
           var hashBottom = this.hash;
-          // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (1200) specifies the number of milliseconds it takes to scroll to the specified area
           $('html, body').animate({
               scrollTop: $(hashBottom).offset().top
             }, 1200, function(){
           });
-        } // End if
+        }
     });
 
       $('.navbar-brand').on('click', function(){
